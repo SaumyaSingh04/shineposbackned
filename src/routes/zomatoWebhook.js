94bb7452b2fc2c2bef461d98d22a920cd7a96700
Sayain-Aditya <as4316664@gmail.com>
2026-01-28 17:27:13 +0530

    sad

--- src/routes/zomatoWebhook.js
+++ src/routes/zomatoWebhook.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getItems, updateCategoryStatus, updateItemStatus } = require('../controllers/zomatoWebhookController');
+const { getItems, postItems, updateCategoryStatus, updateItemStatus } = require('../controllers/zomatoWebhookController');
 
-// Items endpoints
 router.get('/:resId/items', getItems);
-
-// Category status
+router.post('/:resId/items', postItems);
 router.post('/:resId/categories/status', updateCategoryStatus);
-
-// Item status
 router.post('/:resId/items/status', updateItemStatus);
 
 module.exports = router;
